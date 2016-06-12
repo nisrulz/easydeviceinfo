30d2f20d00ac498bfed87785c492c02da4ad6593
Nishant Srivastava <nisrulz@gmail.com>
2016-06-12 21:26:58 +0530

    [*] updated gitignore to include gradle-wrapper.jar

--- app/src/main/java/github/nisrulz/projecteasydeviceinfo/MainActivity.java
+++ app/src/main/java/github/nisrulz/projecteasydeviceinfo/MainActivity.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
   private ArrayAdapter<String> adapter;
-  
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
