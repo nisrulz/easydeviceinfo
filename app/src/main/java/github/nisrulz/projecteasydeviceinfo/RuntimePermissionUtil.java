763aa840d4c08d8bbcf5ff01c16c336c6c38a903
Nishant Srivastava <nisrulz@gmail.com>
2016-10-15 16:06:32 +0530

    [+] added a splash screen and request for runtime permissions to the sample app

---
+++ app/src/main/java/github/nisrulz/projecteasydeviceinfo/RuntimePermissionUtil.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015 - 2016 OmniLabs, Inc.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package github.nisrulz.projecteasydeviceinfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+public class RuntimePermissionUtil {
+
+  private RuntimePermissionUtil() {
+
+  }
+
+  public static void onRequestPermissionsResult(int[] grantResults,
+      RPResultListener RPResultListener) {
+    if (grantResults.length > 0) {
+      for (int i = 0; i < grantResults.length; i++) {
+        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+          RPResultListener.onPermissionGranted();
+        } else {
+          RPResultListener.onPermissionDenied();
+        }
+      }
+    }
+  }
+
+  public static void requestPermission(final Activity activity, final String[] permissions,
+      final int REQUEST_CODE) {
+    // No explanation needed, we can request the permission.
+    ActivityCompat.requestPermissions(activity, permissions, REQUEST_CODE);
+  }
+
+  public static void requestPermission(final Activity activity, final String permission,
+      final int REQUEST_CODE) {
+    // No explanation needed, we can request the permission.
+    ActivityCompat.requestPermissions(activity, new String[] { permission }, REQUEST_CODE);
+  }
+
+  public static boolean checkPermissonGranted(Context context, String permission) {
+    return (ActivityCompat.checkSelfPermission(context, permission)
+        == PackageManager.PERMISSION_GRANTED);
+  }
+}
