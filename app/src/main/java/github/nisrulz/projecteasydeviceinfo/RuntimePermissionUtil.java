3b20ecd168dfb7229d8d014c01d8238eb2751c9d
Nishant Srivastava <nisrulz@gmail.com>
2017-04-23 19:13:13 -0700

    added copyright

--- app/src/main/java/github/nisrulz/projecteasydeviceinfo/RuntimePermissionUtil.java
+++ app/src/main/java/github/nisrulz/projecteasydeviceinfo/RuntimePermissionUtil.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package github.nisrulz.projecteasydeviceinfo;
 
 import android.app.Activity;
